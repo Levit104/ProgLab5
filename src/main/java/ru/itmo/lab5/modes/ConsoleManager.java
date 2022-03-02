91842b0ebd2f6087c33f2613cce2ff0cb67a5e50
Levit104 <artyom0104@gmail.com>
2022-03-03 02:29:59 +0300

    Add better descriptions

--- src/main/java/ru/itmo/lab5/modes/ConsoleManager.java
+++ src/main/java/ru/itmo/lab5/modes/ConsoleManager.java
@@ -19,10 +19,6 @@ public class ConsoleManager {
         this.scanner = scanner;
     }
 
-    public boolean isInScript() {
-        return inScript;
-    }
-
     public void setInScript(boolean inScript) {
         this.inScript = inScript;
     }
@@ -38,6 +34,7 @@ public class ConsoleManager {
     /**
      * Создает новый билет, не добавляя его в коллекцию
      * @param key ключ
+     * @return билет
      */
 
     public Ticket createTicket(Integer key) {
@@ -57,10 +54,12 @@ public class ConsoleManager {
 
     private String askName() {
         while (true) {
-            if (!inScript) System.out.print("Введите название билета: ");
-            String ticketName = scanner.nextLine().trim();
+            if (!inScript) {
+                System.out.print("Введите название билета: ");
+            }
+            String ticketName = scanner.nextLine();
             if (ticketName.isEmpty()) {
-                System.out.println("Значение поля не может быть пустым");
+                System.out.println("Значение названия билета не может быть пустым и не должно содержать пробелов");
             } else {
                 return ticketName;
             }
@@ -70,19 +69,17 @@ public class ConsoleManager {
     private Double askCoordinateX() {
         while (true) {
             try {
-                if (!inScript) System.out.print("Введите координату X (максимальное значение - 606): ");
-                Double ticketCoordinateX = Double.parseDouble(scanner.nextLine().trim());
+                if (!inScript) {
+                    System.out.print("Введите координату X (максимальное значение - 606): ");
+                }
+                Double ticketCoordinateX = Double.parseDouble(scanner.nextLine());
                 if (ticketCoordinateX > 606) {
-                    System.out.println("Максимальное значение координаты - 606");
+                    throw new NumberFormatException();
                 } else {
                     return ticketCoordinateX;
                 }
             } catch (NumberFormatException e) {
-                if (e.getMessage().equals("empty String")) {
-                    System.out.println("Значение поля не может быть пустым");
-                } else {
-                    System.out.println("Необходимо ввести одно число не больше 606");
-                }
+                System.out.println("Значение координаты X должно быть числом не больше 606 и не содержать пробелов");
             }
         }
     }
@@ -90,19 +87,17 @@ public class ConsoleManager {
     private Double askCoordinateY() {
         while (true) {
             try {
-                if (!inScript) System.out.print("Введите координату Y (максимальное значение - 483): ");
-                Double ticketCoordinateY = Double.parseDouble(scanner.nextLine().trim());
+                if (!inScript) {
+                    System.out.print("Введите координату Y (максимальное значение - 483): ");
+                }
+                Double ticketCoordinateY = Double.parseDouble(scanner.nextLine());
                 if (ticketCoordinateY > 483) {
-                    System.out.println("Максимальное значение координаты - 483");
+                    throw new NumberFormatException();
                 } else {
                     return ticketCoordinateY;
                 }
             } catch (NumberFormatException e) {
-                if (e.getMessage().equals("empty String")) {
-                    System.out.println("Значение поля не может быть пустым");
-                } else {
-                    System.out.println("Необходимо ввести одно число не больше 483");
-                }
+                System.out.println("Значение координаты X должно быть числом не больше 483 и не содержать пробелов");
             }
         }
     }
@@ -110,15 +105,17 @@ public class ConsoleManager {
     private double askPrice() {
         while (true) {
             try {
-                if (!inScript) System.out.print("Введите цену билета (значение должно быть больше нуля): ");
-                double ticketPrice = Double.parseDouble(scanner.nextLine().trim());
+                if (!inScript) {
+                    System.out.print("Введите цену билета (значение должно быть больше нуля): ");
+                }
+                double ticketPrice = Double.parseDouble(scanner.nextLine());
                 if (ticketPrice <= 0) {
-                    System.out.println("Значение поля должно быть больше нуля");
+                    throw new NumberFormatException();
                 } else {
                     return ticketPrice;
                 }
             } catch (NumberFormatException e) {
-                System.out.println("Необходимо ввести одно число больше нуля");
+                System.out.println("Значение цены должно быть числом больше нуля и не содержать пробелов");
             }
         }
     }
@@ -126,40 +123,42 @@ public class ConsoleManager {
     private TicketType askTicketType() {
         while (true) {
             try {
-                if (!inScript) System.out.println("Возможные варианты: " + TicketType.valuesToString());
-                if (!inScript) System.out.print("Введите тип билета: ");
-                TicketType ticketType = TicketType.valueOf(scanner.nextLine().trim().toUpperCase());
+                if (!inScript) {
+                    System.out.println("Возможные варианты: " + TicketType.valuesToString());
+                    System.out.print("Введите тип билета: ");
+                }
+                TicketType ticketType = TicketType.valueOf(scanner.nextLine().toUpperCase());
                 return ticketType;
             } catch (IllegalArgumentException e) {
-                if (e.getMessage().equals("No enum constant TicketType.")) {
-                    System.out.println("Значение поля не может быть пустым");
-                } else {
-                    System.out.println("Вы ввели недопустимый тип");
-                }
+                System.out.println("Значение \"тип билета\" должно соответствовать одному из допустимых типов и не содержать пробелов");
             }
         }
     }
 
     private boolean askEvent() {
         while (true) {
-            if (!inScript) System.out.print("Создать событие? (yes/no) ");
-            String choice = scanner.nextLine().trim();
+            if (!inScript) {
+                System.out.print("Создать событие? (yes/no) ");
+            }
+            String choice = scanner.nextLine();
             if (choice.equals("yes")) {
                 return true;
             } else if (choice.equals("no")) {
                 return false;
             } else {
-                System.out.println("Вы ввели недопустимый ответ");
+                System.out.println("Возможны только варианты (yes/no) без пробелов");
             }
         }
     }
 
     private String askEventName() {
         while (true) {
-            if (!inScript) System.out.print("Введите название события: ");
-            String eventName = scanner.nextLine().trim();
+            if (!inScript) {
+                System.out.print("Введите название события: ");
+            }
+            String eventName = scanner.nextLine();
             if (eventName.isEmpty()) {
-                System.out.println("Значение поля не может быть пустым");
+                System.out.println("Значение названия события не может быть пустым и не должно содержать пробелов");
             } else {
                 return eventName;
             }
@@ -169,11 +168,13 @@ public class ConsoleManager {
     private LocalDateTime askEventTime() {
         while (true) {
             try {
-                if (!inScript) System.out.print("Введите дату в формате ЧЧ:мм:сс дд.ММ.гггг: ");
+                if (!inScript) {
+                    System.out.print("Введите дату в формате ЧЧ:мм:сс дд.ММ.гггг: ");
+                }
                 LocalDateTime date = LocalDateTime.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy"));
                 return date;
             } catch (DateTimeParseException e) {
-                System.out.println("Вы ввели недопустимый формат даты");
+                System.out.println("Значение даты должно строго соответствовать формату дату и не содержать пробелов");
             }
         }
     }
@@ -181,16 +182,14 @@ public class ConsoleManager {
     private EventType askEventType() {
         while (true) {
             try {
-                if (!inScript) System.out.println("Возможные варианты: " + EventType.valuesToString());
-                if (!inScript) System.out.print("Введите тип события: ");
-                EventType eventType = EventType.valueOf(scanner.nextLine().trim().toUpperCase());
+                if (!inScript) {
+                    System.out.println("Возможные варианты: " + EventType.valuesToString());
+                    System.out.print("Введите тип события: ");
+                }
+                EventType eventType = EventType.valueOf(scanner.nextLine().toUpperCase());
                 return eventType;
             } catch (IllegalArgumentException e) {
-                if (e.getMessage().equals("No enum constant EventType.")) {
-                    System.out.println("Значение поля не может быть пустым");
-                } else {
-                    System.out.println("Вы ввели недопустимый тип");
-                }
+                System.out.println("Значение \"тип события\" должно соответствовать одному из допустимых типов и не содержать пробелов");
             }
         }
     }
