1d5e22a9cb27878e69441a1df0d3f4ca2afc361f
Levit104 <artyom0104@gmail.com>
2022-03-06 00:06:08 +0300

    Update Main to use new structure

--- src/main/java/ru/itmo/lab5/main/Main.java
+++ src/main/java/ru/itmo/lab5/main/Main.java
@@ -2,22 +2,23 @@ package ru.itmo.lab5.main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-import ru.itmo.lab5.collection.CollectionControl;
+import ru.itmo.lab5.collection.CollectionManager;
 import ru.itmo.lab5.commands.*;
 import ru.itmo.lab5.exceptions.FileFormatException;
-import ru.itmo.lab5.modes.ConsoleMode;
-import ru.itmo.lab5.parser.CSVParser;
+import ru.itmo.lab5.console.CommandManager;
+import ru.itmo.lab5.console.ConsoleManager;
+import ru.itmo.lab5.file.FileManager;
 
 public class Main {
     public static void main(String[] args) {
-        CollectionControl collectionControl = new CollectionControl();
         String file = null;
-
+        
         try {
             if (args.length > 1) {
                 throw new ArrayIndexOutOfBoundsException();
-            } else if (!CSVParser.checkFileExtension(args[0])) {
+            } else if (!FileManager.checkFileExtension(args[0])) {
                 throw new FileFormatException();
             } else if (!new File(args[0]).isFile()) {
                 throw new FileNotFoundException();
@@ -25,34 +26,33 @@ public class Main {
             file = args[0];
         } catch (ArrayIndexOutOfBoundsException e1) {
             System.out.println("Неверно указан путь до файла\n");
-        } catch (IllegalArgumentException e2) {
+        } catch (FileFormatException e2) {
             System.out.println("Неверное расширение файла\n");
         } catch (FileNotFoundException e3) {
             System.out.println("Указан несуществующий файл\n");
         }
 
+        FileManager fileManager = new FileManager(file);
+        CollectionManager collectionControl = new CollectionManager(fileManager);
+        ConsoleManager consoleManager = new ConsoleManager(new Scanner(System.in));
+
         Command[] commands = {
                 new InfoCommand(collectionControl),
                 new ShowCommand(collectionControl),
-                new InsertCommand(collectionControl),
-                new UpdateCommand(collectionControl),
+                new InsertCommand(collectionControl, consoleManager),
+                new UpdateCommand(collectionControl, consoleManager),
                 new RemoveCommand(collectionControl),
                 new ClearCommand(collectionControl),
                 new SaveCommand(collectionControl),
-                // Command executeScriptCommand = new ExecuteScriptCommand(collectionControl),
-                new ReplaceIfGreaterCommand(collectionControl),
-                new ReplaceIfLowerCommand(collectionControl),
+                new ReplaceIfGreaterCommand(collectionControl, consoleManager),
+                new ReplaceIfLowerCommand(collectionControl, consoleManager),
                 new RemoveLowerKeyCommand(collectionControl),
                 new FilterCommand(collectionControl),
                 new PrintAscendingCommand(collectionControl),
                 new PrintFieldDescendingCommand(collectionControl)
         };
 
-        CSVParser csvParser = new CSVParser(file);
-        ConsoleMode consoleMode = new ConsoleMode(commands, file);
-
-        csvParser.parse();
-        collectionControl.addValues(csvParser.getTicketMap());
-        consoleMode.start();
+        CommandManager commandManager = new CommandManager(consoleManager, commands, file);
+        commandManager.consoleMode();
     }
 }
